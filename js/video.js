30ff4bf3b784e622724fc19bca3e4aa8d1132e66
Rodz3rd <rodrigo_galura3rd@yahoo.com>
2015-10-21 01:39:50 +0800

    Modify my age and my parents age

---
+++ js/video.js
@@ -0,0 +1,73 @@
+var vid_open_tag  = '<video id="video" controls>';
+var vid_close_tag = '</video>';
+
+function play1() {
+	video = vid_open_tag + '<source type="video/mp4" src="videos/9GAG - Why So Serious_.mp4" />' + vid_close_tag;
+	document.getElementById("video-container").innerHTML=video;
+}
+
+function play2() {
+	video = vid_open_tag + '<source type="video/webm" src="videos/Anime Face Maker[1].webm" />' + vid_close_tag;
+	document.getElementById("video-container").innerHTML=video;
+}
+
+function play3() {
+	video = vid_open_tag + '<source type="video/mp4" src="videos/(42) Facebook.mp4" />' + vid_close_tag;
+	document.getElementById("video-container").innerHTML=video;
+}
+
+function play4() {
+	video = vid_open_tag + '<source type="video/mp4" src="videos/cratus dance geme geme.mp4" />' + vid_close_tag;
+	document.getElementById("video-container").innerHTML=video;
+}
+
+function play5() {
+	video = vid_open_tag + '<source type="video/mp4" src="videos/Facebook.mp4" />' + vid_close_tag;
+	document.getElementById("video-container").innerHTML=video;
+}
+
+function play6() {
+	video = vid_open_tag + '<source type="video/mp4" src="videos/Kick Ass Vines\'s Videos.mp4" />' + vid_close_tag;
+	document.getElementById("video-container").innerHTML=video;
+}
+
+function play7() {
+	video = vid_open_tag + '<source type="video/mp4" src="videos/Kick Ass Vines\'s Videos_2.mp4" />' + vid_close_tag;
+	document.getElementById("video-container").innerHTML=video;
+}
+
+function play8() {
+	video = vid_open_tag + '<source type="video/mp4" src="videos/Kick Ass Vines\'s Videos_3.mp4" />' + vid_close_tag;
+	document.getElementById("video-container").innerHTML=video;
+}
+
+function play9() {
+	video = vid_open_tag + '<source type="video/mp4" src="videos/LAUGHTRIP! - Anime Face Maker.mp4" />' + vid_close_tag;
+	document.getElementById("video-container").innerHTML=video;
+}
+
+function play10() {
+	video = vid_open_tag + '<source type="video/webm" src="videos/Master Reysan Must watch! Best Combo Ever m.webm" />' + vid_close_tag;
+	document.getElementById("video-container").innerHTML=video;
+}
+
+
+function play11() {
+	video = vid_open_tag + '<source type="video/webm" src="videos/Master Reysan.webm" />' + vid_close_tag;
+	document.getElementById("video-container").innerHTML=video;
+}
+
+function play12() {
+	video = vid_open_tag + '<source type="video/webm" src="videos/SHIT Gusto Ko Happy Ka bola.webm" />' + vid_close_tag;
+	document.getElementById("video-container").innerHTML=video;
+}
+
+function play13() {
+	video = vid_open_tag + '<source type="video/webm" src="videos/SHIT Gusto Ko Happy Ka destiny.webm" />' + vid_close_tag;
+	document.getElementById("video-container").innerHTML=video;
+}
+
+function play14() {
+	video = vid_open_tag + '<source type="video/mp4" src="videos/SHORTCUT XD - Anime Face Maker.mp4" />' + vid_close_tag;
+	document.getElementById("video-container").innerHTML=video;
+}
